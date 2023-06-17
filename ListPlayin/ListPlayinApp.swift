df7b7e9c6638139d3e51e63919d513612f73a865
Mike Kavouras <kavourasm@gmail.com>
2023-06-17 13:55:13 -0500

    Initial Commit

---
+++ ListPlayin/ListPlayinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListPlayinApp.swift
+//  ListPlayin
+//
+//  Created by Mike Kavouras on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ListPlayinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
